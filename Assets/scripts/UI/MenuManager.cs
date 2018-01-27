b7e8f2d5ad45fa688c84b16067cb944dd7908df9
Timo Moisio <timohmoisio@gmail.com>
2018-01-27 14:24:54 +0200

    Add menu

---
+++ Assets/scripts/UI/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour 
+{
+	[SerializeField]
+	private SideMenu sideMenu;
+
+	void Update()
+	{
+		if (Input.GetKeyUp (KeyCode.Escape)) 
+		{
+			if (sideMenu.IsOpen) 
+			{
+				sideMenu.Close();
+			}
+			else 
+			{
+				sideMenu.Open();
+			}
+		}
+	}
+}
