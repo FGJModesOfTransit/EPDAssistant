a47773aee1c43f0f918482a07c17efb6fa4d5303
Timo Moisio <timohmoisio@gmail.com>
2018-01-28 01:13:23 +0200

    Add endscene

---
+++ Assets/scripts/UI/EndScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour 
+{
+	[SerializeField]
+	private Text scoreLabel;
+
+	void Start () 
+	{
+		scoreLabel.text = "Wave X\nTotal Infected X";
+	}
+
+	public void Replay()
+	{
+		SceneManager.LoadScene (1);
+	}
+}
