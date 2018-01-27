bb0aa79edac588a271a8b150c6e0e1d117142769
Joni Räsänen <joni.rasanen@iki.fi>
2018-01-27 22:55:50 +0200

    Limited movement to three nodes.

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -58,11 +58,14 @@ public class Movement : MonoBehaviour
 		if (target.OnRoute) {
 			Debug.Log ("Deleting route");
 			//check if on route and delete route from that point onwards
+
 			if (target == currentNode_) {
 				Debug.Log ("Its the curren node");
 				if (route_.Count > 0) {
 					GraphManager.Instance.GetConnection (currentNode_, 
 						route_ [0]).SetOnPath (false, false);
+
+
 					if (route_.Count > 1) {
 						for (int i = 1; i < route_.Count; ++i) {
 							GraphManager.Instance.GetConnection (route_ [i - 1], 
@@ -100,12 +103,16 @@ public class Movement : MonoBehaviour
             Node prev = route_.Count > 0 ? route_[route_.Count-1] : currentNode_;
             Connection conn = GraphManager.Instance.GetConnection(prev, target);
             conn.SetOnPath(true, conn.m_Node1 == target);
-			route_.Add (target);
+			if (route_.Count < 2) {
+				route_.Add (target);
+			}
 		}
 
 		disableHighlighted ();
 		highlightRoute ();
-		highlightNeighbours ();
+		if (route_.Count < 2) {
+			highlightNeighbours ();
+		}
 	}
 
 	void disableHighlighted()
