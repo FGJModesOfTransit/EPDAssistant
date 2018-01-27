95cad1c5e6cbf5ad83601bcc2459262a703c6ff2
Tiiti Kellomäki <tiiti.kellomaki@student.tut.fi>
2018-01-28 01:02:37 +0200

    Disease names

--- Assets/scripts/UI/ScoreDisplay.cs
+++ Assets/scripts/UI/ScoreDisplay.cs
@@ -11,12 +11,25 @@ public class ScoreDisplay : MonoBehaviour
 	[SerializeField]
 	private Text totalInfectedText;
 
-	void Update()
+    [SerializeField]
+    private Text diseaseNameText;
+
+    void Update()
 	{
 		if (DiseaseManager.Instance != null) 
 		{
-			currentInfectedText.text = "Current Infected:\n" + DiseaseManager.Instance.CountCurrentInflicted();
+            int current = DiseaseManager.Instance.CountCurrentInflicted();
+
+            currentInfectedText.text = "Current Infected:\n" + current;
 			totalInfectedText.text = "Total Infected:\n" + DiseaseManager.Instance.CountTotalInflicted();
+
+            if (current > 0)
+            {
+                diseaseNameText.text = "Current outbreak:" + DiseaseManager.Instance.GetDiseaseName();
+            } else
+            {
+                diseaseNameText.text = "";
+            }
 		}
 	}
 }
