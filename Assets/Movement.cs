844743f04d147d5f6875c5c5785ed118f665e298
Joni Räsänen <joni.rasanen@iki.fi>
2018-01-27 11:43:00 +0200

    Next step in movement logic.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,37 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-	/*
-	public float speed = 1.0f;
-	public GameObject target = null;
-	private Transform t = null;
-*/
+
+	public GameObject currentNode_ = null;
+	bool moving_ = false;
+
+	List<GameObject> route_ = new List<GameObject> ();
 
 	int id = 0;
 	// Use this for initialization
 	void Start () {
-		//t = GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*
-		if (target != null) {
-
-			float movement = 0;
-
-			t.position = Vector2.Lerp (t.position, target.transform.position, Time.deltaTime*speed);
-		}*/
 	}
 
-	public void setTarget(GameObject target)
+	public void AddTarget(GameObject target)
 	{
-		id = LeanTween.moveX(gameObject, 1f, 1f).id;
+		if (currentNode_ == null) {
+			Debug.Log ("Current node not set! Set currrent node in editor.");
+			return;
+		}
+
+		//check if target legal
+//		if (route.Count > 0 && route [route.Count - 1].neighbour (target)
+		//			|| currentNode_.neighbour (target)) {
+		if(true){
+			route_.Add (target);
+			if (!moving_) {
+				moveNext ();
+			}
+		}
+	}
+
+
+	void moveNext()
+	{
+		if (route_.Count == 0) {
+			Debug.Log ("ROute finished");
+			moving_ = false;
+			return;
+		}
+		moving_ = true;
+		GameObject nextNode = route_ [0];
+		route_.RemoveAt (0);
+
+		//TODO: get route speed
+		float routeSpeed = 1.0f;
+		id = LeanTween.move(gameObject, nextNode.transform, routeSpeed).id;
 		LTDescr d = LeanTween.descr( id );
 
 		if(d!=null){ // if the tween has already finished it will return null
 			// change some parameters
-			//d.setOnComplete( onCompleteFunc ).setEase( LeanTweenType.easeInOutBack );
+			d.setOnComplete( moveNext );
 		}
+
+		currentNode_ = nextNode;
 	}
 }
