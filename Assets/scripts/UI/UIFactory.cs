0814cd0790a4da38c04daef7d26ceef3d58d122e
Timo Moisio <timohmoisio@gmail.com>
2018-01-27 17:48:56 +0200

    Add UIFactory

---
+++ Assets/scripts/UI/UIFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFactory : MonoBehaviour 
+{
+	[SerializeField]
+	private List<GameObject> requiredPrefabs;
+
+	void Awake()
+	{
+		foreach (var prefab in requiredPrefabs) 
+		{
+			Instantiate (prefab);
+		}
+	}
+}
