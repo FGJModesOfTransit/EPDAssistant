9c961af9bdb8dc6d4b0f390a0c43032da72a6f64
Timo Moisio <timohmoisio@gmail.com>
2018-01-27 16:05:23 +0200

    Add MessageManager

--- Assets/scripts/logic/DiseaseManager.cs
+++ Assets/scripts/logic/DiseaseManager.cs
@@ -9,6 +9,8 @@ public class DiseaseManager : MonoBehaviour
     public Image ImagePrefab;
     public Gradient DiseaseColor;
 
+	public static Action<Node, Disease> OnDiseaseAdded;
+
     private Canvas m_DiseaseCanvas;
 
     public static DiseaseManager Instance
@@ -42,12 +44,16 @@ public class DiseaseManager : MonoBehaviour
     public void AddDisease()
     {
         Node n = GraphManager.Instance.GetRandomNode();
-        Disease oldDisease = n.GetComponent<Disease>();
-        if (oldDisease == null)
+        Disease disease = n.GetComponent<Disease>();
+		if (disease == null)
         {
             Debug.Log("Added disease to node " + n.name);
-            n.gameObject.AddComponent<Disease>();
+			disease = n.gameObject.AddComponent<Disease>();
         }
+		if (OnDiseaseAdded != null) 
+		{
+			OnDiseaseAdded (n, disease);
+		}
     }
 
 	void Update ()
